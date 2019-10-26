f88b96ed7455bac46672f3412f0b6bedcb56c66f
Sky1inePI <skyline14593@gmail.com>
2019-10-26 20:18:13 +0300

    Ping-Pong

---
+++ Gachimuchenik/Gachimuchenik/Modules/Commands.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Discord;
+using Discord.Commands;
+using Discord.WebSocket;
+
+namespace Gachimuchenik.Modules
+{    public class Commands : ModuleBase<SocketCommandContext>
+    {
+        [Command("SLAVE")]
+        public async Task Ping() {
+            await Context.Channel.SendMessageAsync($"{Context.User.Mention} ♂ YES SIR ♂");
+        }
+    }
+}
